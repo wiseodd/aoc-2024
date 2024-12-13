2f79b2b7c12ff6bab7490c6d08627b39eebfc42b
Agustinus Kristiadi <agustinus@kristia.de>
2024-12-13 15:21:09 -0500

    Add numpy solutions for day 13

--- src/day13.py
+++ src/day13.py
@@ -3,32 +3,38 @@ import numpy as np
 
 
 def main():
-    arm_costs = np.array([3, 1], dtype=np.int32)
-
     with open("data/day13_input.txt", "r") as f:
-        # with open("data/day13_input_toy.txt", "r") as f:
         content = f.read().strip()
 
-    total_cost = 0
+    total_part1 = 0
+    total_part2 = 0
 
     for block in content.split("\n\n"):
         [button_a, button_b, prize] = block.splitlines()
-        a11, a21 = extract_button(button_a)
-        a12, a22 = extract_button(button_b)
-        b1, b2 = extract_prize(prize)
+        a1, a2 = extract_button(button_a)
+        b1, b2 = extract_button(button_b)
+        p1, p2 = extract_prize(prize)
 
-        x = np.linalg.solve(
-            np.array([[a11, a12], [a21, a22]], dtype=np.int32),
-            np.array([b1, b2], dtype=np.int32),
+        total_part1 += np.array([3, 1], dtype=int) @ sol_numpy(
+            a1, a2, b1, b2, p1, p2, offset=0
+        )
+        total_part2 += np.array([3, 1], dtype=int) @ sol_numpy(
+            a1, a2, b1, b2, p1, p2, offset=10000000000000
         )
 
-        print(x)
+    print(total_part1)
+    print(total_part2)
 
-        if np.allclose(x.round(), x):
-            # print(x)
-            total_cost += x.astype(np.int32) @ arm_costs
 
-    print(total_cost)
+def sol_numpy(a1, a2, b1, b2, p1, p2, offset=0):
+    A = np.array([[a1, b1], [a2, b2]])
+    b = np.array([p1 + offset, p2 + offset])
+    x = np.linalg.solve(A, b)
+
+    if round(x[0], 2).is_integer() and round(x[1], 2).is_integer():
+        return x.round().astype(int)
+    else:
+        return np.zeros(2, dtype=int)
 
 
 def extract_button(line: str) -> Tuple[int, int]:
